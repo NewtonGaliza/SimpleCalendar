24ed69c1af776c831e4708b7cdf77bd950875a2f
Newton Galiza <newtonjgaliza@gmail.com>
2018-07-17 17:53:54 -0300

    Create simpleCalendar.py

---
+++ simpleCalendar.py
@@ -0,0 +1,14 @@
+import calendar
+cal = calendar.month(2018, 6)
+print (cal)
+
+'''
+Output
+ June 2018
+Mo Tu We Th Fr Sa Su
+             1  2  3
+ 4  5  6  7  8  9 10
+11 12 13 14 15 16 17
+18 19 20 21 22 23 24
+25 26 27 28 29 30
+'''
